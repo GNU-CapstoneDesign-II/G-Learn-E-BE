19905891480c00668faed1bbe6c972514a11b04d
circlezero <wy129@naver.com>
2025-03-20 19:20:47 +0900

    [Feat] 프로젝트 초기화-#1

---
+++ src/main/java/gnu/capstone/G_Learn_E/GLearnEApplication.java
@@ -0,0 +1,13 @@
+package gnu.capstone.G_Learn_E;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GLearnEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GLearnEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gnu/capstone/G_Learn_E/GLearnEApplicationTests.java
@@ -0,0 +1,13 @@
+package gnu.capstone.G_Learn_E;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GLearnEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
