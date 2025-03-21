0e1e4b1a4e95f2c44649edfe9e175eda6a82722e
circlezero <wy129@naver.com>
2025-03-22 02:57:28 +0900

    [Feat #3] 광역 예외 처리 시스템 생성

---
+++ src/main/java/gnu/capstone/G_Learn_E/global/error/exception/NotFoundGroupException.java
@@ -0,0 +1,7 @@
+package gnu.capstone.G_Learn_E.global.error.exception;
+
+public abstract class NotFoundGroupException extends RuntimeException {
+    public NotFoundGroupException(String message) {
+        super(message);
+    }
+}
