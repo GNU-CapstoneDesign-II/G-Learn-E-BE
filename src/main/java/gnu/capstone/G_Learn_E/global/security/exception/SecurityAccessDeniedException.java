12f4953bc71bab9ceade469da6f310150cc68c03
circlezero <wy129@naver.com>
2025-03-25 23:50:10 +0900

    [Refactor #17] 인증된 사용자에 대한 미인증 전용 URL 예외응답 추가

--- src/main/java/gnu/capstone/G_Learn_E/global/security/exception/SecurityAccessDeniedException.java
+++ src/main/java/gnu/capstone/G_Learn_E/global/security/exception/SecurityAccessDeniedException.java
@@ -10,4 +10,8 @@ public class SecurityAccessDeniedException extends AccessDeniedGroupException {
     public static SecurityAccessDeniedException accessDenied() {
         return new SecurityAccessDeniedException("해당 요청에 대한 권한이 없습니다.");
     }
+
+    public static SecurityAccessDeniedException alreadyAuthenticated() {
+        return new SecurityAccessDeniedException("이미 인증된 사용자입니다.");
+    }
 }
