1482fffc0cd06bac2971071e149fc8e852bd7872
pjw <m11d29v3p@naver.com>
2025-04-01 21:13:39 +0900

    [Feat #18] 과목, 문제집 목록 조회 api 개발

--- src/main/java/gnu/capstone/G_Learn_E/domain/public_folder/repository/SubjectWorkbookMapRepository.java
+++ src/main/java/gnu/capstone/G_Learn_E/domain/public_folder/repository/SubjectWorkbookMapRepository.java
@@ -1,9 +1,13 @@
 package gnu.capstone.G_Learn_E.domain.public_folder.repository;
 
+import gnu.capstone.G_Learn_E.domain.public_folder.entity.SubjectWorkbookId;
 import gnu.capstone.G_Learn_E.domain.public_folder.entity.SubjectWorkbookMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SubjectWorkbookMapRepository extends JpaRepository<SubjectWorkbookMap, Long> {
+public interface SubjectWorkbookMapRepository extends JpaRepository<SubjectWorkbookMap, SubjectWorkbookId> {
+    List<SubjectWorkbookMap> findAllBySubject_Id(Long subjectId);
 }
