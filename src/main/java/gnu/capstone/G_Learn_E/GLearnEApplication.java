083f4e0f35340a0723f6663a4adf92b41843213c
circlezero <wy129@naver.com>
2025-04-03 00:36:16 +0900

    [Feat #25] Json 파일 업로드를 통한 퍼블릭 폴더 내용 생성 기능 추가

--- src/main/java/gnu/capstone/G_Learn_E/GLearnEApplication.java
+++ src/main/java/gnu/capstone/G_Learn_E/GLearnEApplication.java
@@ -2,8 +2,10 @@ package gnu.capstone.G_Learn_E;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class GLearnEApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/gnu/capstone/G_Learn_E/global/file_upload/controller/FileUploadController.java
@@ -0,0 +1,48 @@
+package gnu.capstone.G_Learn_E.global.file_upload.controller;
+
+import gnu.capstone.G_Learn_E.global.file_upload.service.FileUploadService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/dev/api/fileupload")
+public class FileUploadController {
+
+    private final FileUploadService fileUploadService;
+
+
+    @PostMapping("/upload/public_folder_metadata")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            String originalFilename = file.getOriginalFilename();
+            long size = file.getSize();
+            System.out.println("파일명: " + originalFilename + ", 크기: " + size);
+
+            // 1. 임시 폴더 생성 및 파일 저장
+            Path tempDir = Files.createTempDirectory("");
+            Path tempFilePath = tempDir.resolve(originalFilename);
+            file.transferTo(tempFilePath.toFile());
+
+            // 2. 비동기로 파일 파싱 + DB 적재
+            fileUploadService.processUploadedFile(tempFilePath);
+
+            return ResponseEntity.ok("파일 업로드 성공, 비동기 처리 시작: " + originalFilename);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("업로드 실패: " + e.getMessage());
+        }
+    }
+}
