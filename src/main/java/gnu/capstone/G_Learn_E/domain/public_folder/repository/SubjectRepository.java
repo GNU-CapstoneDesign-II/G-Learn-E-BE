fab4907ecdb101f6f22c20efcc9ee5f565b01ae2
circlezero <wy129@naver.com>
2025-04-03 00:36:05 +0900

    [Feat #25] 퍼블릭 폴더 Repository 메서드 추가

--- src/main/java/gnu/capstone/G_Learn_E/domain/public_folder/repository/SubjectRepository.java
+++ src/main/java/gnu/capstone/G_Learn_E/domain/public_folder/repository/SubjectRepository.java
@@ -1,6 +1,5 @@
 package gnu.capstone.G_Learn_E.domain.public_folder.repository;
 
-import gnu.capstone.G_Learn_E.domain.public_folder.entity.Department;
 import gnu.capstone.G_Learn_E.domain.public_folder.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    boolean existsByNameAndDepartmentId(String name, Long departmentId);
     List<Subject> findByDepartmentId(Long departmentId);
 }
