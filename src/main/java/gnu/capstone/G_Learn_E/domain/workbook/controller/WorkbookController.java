a39171518d0149a7af8558f93f7d4d2cc5800427
circlezero <wy129@naver.com>
2025-04-12 19:01:10 +0900

    [Refactor #41] 문제 풀이 페이지 API 도메인 변경

--- src/main/java/gnu/capstone/G_Learn_E/domain/workbook/controller/WorkbookController.java
+++ src/main/java/gnu/capstone/G_Learn_E/domain/workbook/controller/WorkbookController.java
@@ -1,5 +1,12 @@
 package gnu.capstone.G_Learn_E.domain.workbook.controller;
 
+import gnu.capstone.G_Learn_E.domain.workbook.converter.WorkbookConverter;
+import gnu.capstone.G_Learn_E.domain.workbook.dto.response.WorkbookSolveResponse;
+import gnu.capstone.G_Learn_E.domain.problem.entity.Problem;
+import gnu.capstone.G_Learn_E.domain.problem.service.ProblemService;
+import gnu.capstone.G_Learn_E.domain.solve_log.entity.SolveLog;
+import gnu.capstone.G_Learn_E.domain.solve_log.entity.SolvedWorkbook;
+import gnu.capstone.G_Learn_E.domain.solve_log.service.SolveLogService;
 import gnu.capstone.G_Learn_E.domain.user.entity.User;
 import gnu.capstone.G_Learn_E.domain.workbook.dto.request.ProblemGenerateRequest;
 import gnu.capstone.G_Learn_E.domain.workbook.dto.response.WorkbookResponse;
@@ -15,6 +22,9 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/workbook")
@@ -22,6 +32,8 @@ import org.springframework.web.bind.annotation.*;
 public class WorkbookController {
 
     private final WorkbookService workbookService;
+    private final ProblemService problemService;
+    private final SolveLogService solveLogService;
     private final FastApiService fastApiService;
 
     // TODO : 문제집 컨트롤러 구현
@@ -43,5 +55,35 @@ public class WorkbookController {
 
         // Workbook 생성 로직 처리 후 결과 반환
         return new ApiResponse<>(HttpStatus.OK, "문제집 생성에 성공하였습니다.", response);
+    }
+
+    /**
+     * 문제 풀이 페이지 로드
+     */
+    @GetMapping("/{workbookId}/solve")
+    public ApiResponse<WorkbookSolveResponse> problemSolvePageLoad(
+            @AuthenticationPrincipal User user,
+            @PathVariable("workbookId") Long workbookId
+    ){
+        Workbook workbook = workbookService.findWorkbookById(workbookId);
+        log.info("문제집 조회 성공 : {}", workbook);
+
+        List<Problem> problems = problemService.findAllByWorkbookId(workbook);
+        log.info("문제 조회 성공 : {}", problems);
+
+        SolvedWorkbook solvedWorkbook = solveLogService.findSolvedWorkbook(workbook, user);
+        log.info("문제집 풀이 기록 조회 성공 : {}", solvedWorkbook);
+
+        Map<Long, SolveLog> solveLogToMap = solveLogService.findAllSolveLogToMap(solvedWorkbook);
+        log.info("문제 풀이 기록 조회 성공 : {}", solveLogToMap);
+
+        WorkbookSolveResponse response = WorkbookConverter.convertToWorkbookSolveResponse(
+                workbook,
+                problems,
+                solvedWorkbook,
+                solveLogToMap
+        );
+        log.info("문제 풀이 페이지 로드 성공 : {}", response);
+        return new ApiResponse<>(HttpStatus.OK, "문제 풀이 페이지 로드 성공", response);
     }
 }
